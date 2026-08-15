4d349f4f79e777865c785240e7a12a4175c77409
darkboy0p <231809516+darkboy0p@users.noreply.github.com>
2026-08-15 08:59:35 +0600

    Delete minecraft-server-utility directory

--- minecraft-server-utility/java/src/main/java/com/minecraftutility/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Minecraft Server Utility - Java implementation.
- * Provides server status pinging (Java Edition) and Mojang API
- * player lookups (UUID, profile, skin/cape URLs).
- */
-package com.minecraftutility;
--- minecraft-server-utility/java/src/test/java/com/minecraftutility/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Test package for Minecraft Server Utility Java implementation.
- * Contains unit tests for all Java components.
- */
-package com.minecraftutility;
